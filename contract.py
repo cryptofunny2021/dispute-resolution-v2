bccfa5c5fb27bc57456f7f97d8115fdf827b54dd
cryptofunny724 <cryptofunny724@gmail.com>
2026-06-15 13:27:15 +0200

    Add contract.py v2.0 - Dispute Resolution

--- contract.py
+++ contract.py
@@ -1,5 +1,5 @@
-# Dispute Resolution v2.0
-# Fair Dispute System with Reputation Impact
+# v2.0
+# { "Depends": "py-genlayer:1jb45aa8ynh2a9c9xn3b7qqh8sm5q93hwfp7jqmwsfhh8jpz09h6" }
 
 from genlayer import *
 
@@ -7,23 +7,23 @@ class DisputeResolution(gl.Contract):
     disputes: TreeMap[Address, str]
     reputation_address: Address
 
-    def __init__(self, rep_addr: Address):
-        self.reputation_address = rep_addr
+    def __init__(self):
+        # Hardcoded reputation address
+        self.reputation_address = Address("0x77f86b0D8A0230BD612F41F9c638d682f6aBc476")
 
     @gl.public.write
     def submit_dispute(self, description: str) -> None:
-        """Submit a new dispute"""
         user = gl.message.sender_address
         self.disputes[user] = description
         
-        # Small reputation penalty for submitting dispute
+        # Small reputation penalty
         try:
             rep = gl.contract(self.reputation_address)
-            rep.record_action(u256(-5))  # Negative impact
+            rep.record_action(u256(-5))
         except:
             pass
 
     @gl.public.view
     def my_dispute(self) -> str:
-        """Get my submitted dispute"""
-        return self.disputes.get(gl.message.sender_address, "")
+        user = gl.message.sender_address
+        return self.disputes.get(user, "")
