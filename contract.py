230189bd1fd53bb2758e7c6f47d57679065ab53a
cryptofunny724 <cryptofunny724@gmail.com>
2026-06-15 16:48:38 +0200

    Update contract.py to final stable v2.5 version

--- contract.py
+++ contract.py
@@ -1,29 +1,29 @@
-# v2.0
+# v2.5 Stable
 # { "Depends": "py-genlayer:1jb45aa8ynh2a9c9xn3b7qqh8sm5q93hwfp7jqmwsfhh8jpz09h6" }
 
 from genlayer import *
 
 class DisputeResolution(gl.Contract):
     disputes: TreeMap[Address, str]
-    reputation_address: Address
+    scores: TreeMap[Address, u256]
 
     def __init__(self):
-        # Hardcoded reputation address
-        self.reputation_address = Address("0x77f86b0D8A0230BD612F41F9c638d682f6aBc476")
+        pass
 
     @gl.public.write
     def submit_dispute(self, description: str) -> None:
         user = gl.message.sender_address
         self.disputes[user] = description
         
-        # Small reputation penalty
-        try:
-            rep = gl.contract(self.reputation_address)
-            rep.record_action(u256(-5))
-        except:
-            pass
+        current = self.scores.get(user, u256(0))
+        self.scores[user] = current - u256(5)
 
     @gl.public.view
     def my_dispute(self) -> str:
         user = gl.message.sender_address
-        return self.disputes.get(user, "")
+        return self.disputes.get(user, "No dispute")
+
+    @gl.public.view
+    def my_score(self) -> u256:
+        user = gl.message.sender_address
+        return self.scores.get(user, u256(0))
